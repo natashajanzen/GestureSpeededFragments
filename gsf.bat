88ffd0c291bc563b19a5dd9de75589e9e7cefb10
Luke Tudge <luketudge@gmail.com>
2019-08-01 21:31:06 +0200

    Delete gsf.bat

--- gsf.bat
+++
@@ -1,5 +0,0 @@
-REM Windows batch file to run the experiment.
-REM Assumes that the path to Python 3 has been added to Windows' environment variables,
-REM which may or may not be the case depending on how Python was installed.
-REM This is currently untested; I have no idea whether it works.
-python gsf.py
